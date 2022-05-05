f50a1daf1eda94e09893e086e51f3c5f54edcb01
tfs-apps-one <furu12080205@gmail.com>
2022-05-05 19:45:14 +0900

    プロジェクト新規作成

---
+++ app/src/main/java/security/alarm/CrSetFragment.java
@@ -0,0 +1,14 @@
+package security.alarm;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+/**
+ * Created by FURUKAWA on 2016/09/30.
+ */
+public class CrSetFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
---
+++ app/src/main/java/security/alarm/MainActivity.java
@@ -0,0 +1,29 @@
+package security.alarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //  ボタン：効果音スタート＆ストップ
+    public void onStartStop(View view) {
+    }
+
+    //  ボタン：スピーカー
+    public void onSpeaker(View view) {
+    }
+
+    //  ボタン：メール送信
+    public void onImgMail(View view) {
+    }
+
+
+}
